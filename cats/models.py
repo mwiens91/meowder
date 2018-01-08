8853bcc6d35c823d0bcf63eb0b548073547d4a1c
Matt Wiens <mwiens91@gmail.com>
2018-01-08 12:00:57 -0800

    Add Profile model

--- cats/models.py
+++ cats/models.py
@@ -1,5 +1,12 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+class Profile(models.Model):
+    """A user profile."""
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    location = models.CharField(max_length=30, null=True, blank=True)
 
 class Cat(models.Model):
     """A cat."""
@@ -10,7 +17,7 @@ class Cat(models.Model):
                                     ('M', 'M'),
                                     ('X', 'X'),),
                            default='F',)
-    owner = models.ForeignKey(User,
+    owner = models.ForeignKey(Profile,
                               on_delete=models.CASCADE,)
     votes = models.ManyToManyField('self',
                                    through='Vote',
@@ -20,7 +27,6 @@ class Cat(models.Model):
 
     def __str__(self):
         return "%s [%s, %s] " % (self.name, self.sex, self.breed)
-
 
 class Vote(models.Model):
     """A 'like' upvote or downvote."""
@@ -39,3 +45,9 @@ class Vote(models.Model):
                               on_delete=models.CASCADE,
                               related_name="votee",
                               default=0,)
+
+@receiver(post_save, sender=User)
+def update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    instance.profile.save()
