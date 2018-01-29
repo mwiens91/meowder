24e1bee001e821912f1ba2f22fa1f765b6d3713f
Matt Wiens <mwiens91@gmail.com>
2018-01-29 14:41:11 -0800

    Don't log 404 when browser requests favicon

--- meowder/settings.py
+++ meowder/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import re
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -131,3 +132,8 @@ LOGIN_URL = 'login'
 
 # Where to go after logging in
 LOGIN_REDIRECT_URL = 'home'
+
+# Don't log 404 when browser requests favicon
+IGNORABLE_404_URLS = [
+    re.compile(r'^/favicon\.ico$'),
+]
