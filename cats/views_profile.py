6acbee2479570003753aa10693c4c1f563c7e3b9
Matt Wiens <mwiens91@gmail.com>
2018-02-05 19:29:17 -0800

    Change "match history"->"matches" + improve dates
    These should be separate commits, but was too difficult to untangle
    them.

--- cats/views_profile.py
+++ cats/views_profile.py
@@ -1,10 +1,12 @@
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.utils import timezone
 from django.views.generic.edit import UpdateView
 from django.shortcuts import redirect, render
 from cats.forms import ProfileSignUpForm
 from cats.models import Profile, Match
+import datetime
 
 
 class EditEmail(UpdateView):
@@ -33,31 +35,44 @@ def home(request):
     return render(request, 'home.html', {'cats': cats})
 
 @login_required
-def match_history(request):
+def matches(request):
     """The page showing a user's cat's matches.
 
     Renders a page with all of a user's cat's matches. Passes along a
     dictionary containing dates as keys, and the matches as values.
+    Also passes along two strings indicating the current date and
+    yesterday's date.
     """
     match_dict = dict()
     matches = Match.objects.filter(
                 matchingcat__owner__id=request.user.profile.id).order_by(
                 '-time')
 
+    # Create a helper function to convert date objects to strings
+    def dateStringify(dateobj):
+        dateString = (str(dateobj.year)
+                      + '-'
+                      + str(dateobj.month).zfill(2)
+                      + '-'
+                      + str(dateobj.day).zfill(2)
+                     )
+        return dateString
+
+    # Create a string for today's date and yesterday's date
+    todayString = dateStringify(timezone.now())
+    yesterdayString = dateStringify(timezone.now() - datetime.timedelta(days=1))
+
     # Build dictionary by using each match's date as a key
     for match in matches:
-        datestring = (str(match.time.year)
-                      + '-'
-                      + str(match.time.month).zfill(2)
-                      + '-'
-                      + str(match.time.day).zfill(2)
-                     )
+        datestring = dateStringify(match.time)
         if datestring in match_dict:
             match_dict[datestring] += [match]
         else:
             match_dict[datestring] = [match]
 
-    return render(request, 'match_history.html', {'matchdict': match_dict})
+    return render(request, 'matches.html', {'matchdict': match_dict,
+                                            'todaydate': todayString,
+                                            'yesterdaydate': yesterdayString})
 
 @login_required
 def profile_edit(request):
