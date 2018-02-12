440f8252a969a489f735fbd0a793c335967d8019
Matt Wiens <mwiens91@gmail.com>
2018-02-11 22:36:18 -0800

    Wait for server response before removing match

--- static/match_dismissal_handler.js
+++ static/match_dismissal_handler.js
@@ -13,24 +13,24 @@ document.body.addEventListener('submit', function (event) {
       'accept': 'application/json'
     },
     method: 'POST'
+  }).then(function () {
+    // Update the UI
+    var parentBox = form.closest('div.matchbox')
+    var boxesWrapper = form.closest('div.matchwrapper')
+    var dateSection = form.closest('div.matchdatesection')
+
+    // Remove the match box
+    parentBox.parentNode.removeChild(parentBox)
+
+    // If no match boxes in date section, remove the date section
+    if (!boxesWrapper.children.length) {
+      dateSection.parentNode.removeChild(dateSection)
+    }
+
+    // If no matches left at all, show "no matches" text
+    if (!document.getElementsByClassName('matchdatesection').length) {
+      document.getElementById('has-matches').style.display = 'none'
+      document.getElementById('no-matches').style.display = 'block'
+  }
   })
-
-  // Update the UI
-  var parentBox = form.closest('div.matchbox')
-  var boxesWrapper = form.closest('div.matchwrapper')
-  var dateSection = form.closest('div.matchdatesection')
-
-  // Remove the match box
-  parentBox.parentNode.removeChild(parentBox)
-
-  // If no match boxes in date section, remove the date section
-  if (!boxesWrapper.children.length) {
-    dateSection.parentNode.removeChild(dateSection)
-  }
-
-  // If no matches left at all, show "no matches" text
-  if (!document.getElementsByClassName('matchdatesection').length) {
-    document.getElementById('has-matches').style.display = 'none'
-    document.getElementById('no-matches').style.display = 'block'
-  }
 })
