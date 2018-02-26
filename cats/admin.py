bde9c8966d6ef2f9551dedd376b586e3a529dcfb
Matt Wiens <mwiens91@gmail.com>
2018-02-25 18:46:35 -0800

    Add admin interface to Votes and Matches

--- cats/admin.py
+++ cats/admin.py
@@ -24,5 +24,15 @@ class ProfileAdmin(admin.ModelAdmin):
     """Interface modifiers for the Profile model for the admin page."""
     list_display = ('__str__', 'location', 'timezone')
 
+class VoteAdmin(admin.ModelAdmin):
+    """Interface modifiers for the Vote model for the admin page."""
+    list_display = ('voter', 'votee', 'value')
+
+class MatchAdmin(admin.ModelAdmin):
+    """Interface modifiers for the Vote model for the admin page."""
+    list_display = ('matchingcat', 'matchedcat', 'time', 'seen')
+
 admin.site.register(Cat, CatAdmin)
 admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Vote, VoteAdmin)
+admin.site.register(Match, MatchAdmin)
